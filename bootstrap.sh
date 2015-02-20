3e92e5e303b79c6335bca1a1aa80f196ccc4d755
Rodrigo Prado <royopa@gmail.com>
2015-02-20 20:29:50 -0200

    updated repo to dspace 4.2 auto install

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,8 @@
 apt-get update
 apt-get install -y language-pack-en software-properties-common python-software-properties wget git unzip linux-headers-generic build-essential dkms
 
-wget https://github.com/royopa/dspace-auto-install/archive/master.zip
-unzip master.zip
+wget https://github.com/royopa/dspace-auto-install/archive/dspace-4.2.zip
+unzip dspace-4.2.zip 
 
-cd dspace-auto-install-master
+cd dspace-auto-install-dspace-4.2
 ./build-dspace
