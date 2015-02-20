6b1389879d83892845afcfcc6663168946b2200f
Rodrigo Prado <royopa@gmail.com>
2015-02-20 20:33:27 -0200

    updated repo to dspace 5.0 auto install

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,8 @@
 apt-get update
 apt-get install -y language-pack-en software-properties-common python-software-properties wget git unzip linux-headers-generic build-essential dkms
 
-https://github.com/royopa/dspace-auto-install/archive/dspace-5.x.zip
-unzip master.zip
+wget https://github.com/royopa/dspace-auto-install/archive/dspace-5.x.zip
+unzip dspace-5.x.zip
 
-cd dspace-auto-install-master
+cd dspace-auto-install-dspace-5.x
 ./build-dspace
