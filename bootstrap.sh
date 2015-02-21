1a5657ac0b43b5e122ef955664ef144dd16416c9
Rodrigo Prado <royopa@gmail.com>
2015-02-20 22:49:30 -0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get install -y wget git unzip
 
 wget https://github.com/royopa/dspace-auto-install/archive/master.zip
-unzip master.zip
+sudo unzip master.zip
 
 cd dspace-auto-install-master
 ./build-dspace
