5ea9059dd3d07d608e4c6b5f01025abad1419f45
Rodrigo Prado <royopa@gmail.com>
2015-02-21 10:04:25 -0200

    chown in folder

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 sudo apt-get install -y software-properties-common python-software-properties
-sudo apt-get install -y git wget openjdk-7-jdk 
+sudo apt-get install -y git wget unzip mc openjdk-7-jdk
 
 sudo add-apt-repository "deb http://ppa.launchpad.net/natecarlson/maven3/ubuntu precise main"
 sudo apt-get update
@@ -11,5 +11,6 @@ sudo apt-get install -y maven3
 sudo rm -rf dspace-auto-install
 
 git clone -b master https://github.com/royopa/dspace-auto-install.git
+sudo chown vagrant dspace-auto-install
 cd dspace-auto-install
 ./build-dspace
