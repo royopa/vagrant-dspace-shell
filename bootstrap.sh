6fa5826ad72dff068a21c4eaa214a7ea5db5f28b
Rodrigo <royopa@gmail.com>
2015-02-21 09:25:23 -0200

    changed box

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get install -y git
+sudo apt-get install -y software-properties-common python-software-properties
+sudo apt-get install -y git openjdk-7-jdk 
+
+sudo add-apt-repository "deb http://ppa.launchpad.net/natecarlson/maven3/ubuntu precise main"
+sudo apt-get update
+sudo apt-get install -y maven3
 
 sudo rm -rf dspace-auto-install
-sudo rm -rf /dspace
 
 git clone -b dspace-5.x https://github.com/royopa/dspace-auto-install.git
 cd dspace-auto-install
-./build-dspace
+#./build-dspace
