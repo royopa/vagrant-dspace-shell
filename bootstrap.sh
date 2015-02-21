dfdf02cbfd9bdb8a007e1207178e02bf04223718
Rodrigo Prado <royopa@gmail.com>
2015-02-21 09:20:50 -0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y wget git unzip
+sudo apt-get update
+sudo apt-get install -y wget git unzip openjdk-7-jdk ant ant-optional tomcat7 postgresql tomcat7-admin
+
+sudo add-apt-repository "deb http://ppa.launchpad.net/natecarlson/maven3/ubuntu precise main"
+sudo apt-get update
+sudo apt-get install -y maven3
 
 wget https://github.com/royopa/dspace-auto-install/archive/master.zip
 sudo unzip master.zip
