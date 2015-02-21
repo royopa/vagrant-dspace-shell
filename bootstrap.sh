f36e8a89e9d8fa6726b36e75407ecd057cfad688
Rodrigo Prado <royopa@gmail.com>
2015-02-21 09:28:30 -0200

    changed branch to master in dspace-auto-install

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get install -y wget git unzip openjdk-7-jdk ant ant-optional tomcat7 postgresql tomcat7-admin
+sudo apt-get install -y software-properties-common python-software-properties
+sudo apt-get install -y git wget openjdk-7-jdk 
 
 sudo add-apt-repository "deb http://ppa.launchpad.net/natecarlson/maven3/ubuntu precise main"
 sudo apt-get update
 sudo apt-get install -y maven3
 
-git clone -b dspace-5.x https://github.com/royopa/dspace-auto-install.git
+sudo rm -rf dspace-auto-install
+
+git clone -b master https://github.com/royopa/dspace-auto-install.git
 cd dspace-auto-install
 ./build-dspace
