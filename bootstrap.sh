1e2c903d5adfd85f39eb26eb9cf8faf5dc4e0b21
Rodrigo Prado <royopa@gmail.com>
2015-02-21 01:08:02 -0200

    add cachier plugin

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-apt-get update
 apt-get install -y wget git unzip
+
+sudo rm -rf dspace-4.2*.zip
+sudo rm -rf dspace-auto-install*
+sudo rm -rf /dspace
 
 wget https://github.com/royopa/dspace-auto-install/archive/dspace-4.2.zip
 unzip dspace-4.2.zip 
