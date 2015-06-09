4ccad8da140e5d42d0ecfe422ecaf0bed8fd0ed2
Rodrigo Prado <royopa@gmail.com>
2015-06-08 21:48:47 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,13 +3,8 @@
 sudo locale-gen en_US en_US.UTF-8 pt_BR.UTF-8
 sudo dpkg-reconfigure locales
 
-sudo apt-get update
 sudo apt-get install -y software-properties-common python-software-properties
 sudo apt-get install -y git wget unzip mc openjdk-7-jdk
-
-sudo add-apt-repository "deb http://ppa.launchpad.net/natecarlson/maven3/ubuntu precise main"
-sudo apt-get update
-sudo apt-get install -y maven3
 
 sudo rm -rf dspace-auto-install
 
