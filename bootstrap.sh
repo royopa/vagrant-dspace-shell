cba859c52615e99fcaa396d0c34881df162c3fe8
Rodrigo Prado <royopa@gmail.com>
2015-06-08 21:35:17 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+sudo locale-gen en_US en_US.UTF-8 pt_BR.UTF-8
+sudo dpkg-reconfigure locales
+
 sudo apt-get update
 sudo apt-get install -y software-properties-common python-software-properties
 sudo apt-get install -y git wget unzip mc openjdk-7-jdk
